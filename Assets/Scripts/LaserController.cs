27a2d85918d47703e65d5014eeea095bec1acfeb
Constantinos Vafias <cv2134@nyu.edu>
2025-03-27 19:07:35 -0400

    Added telegraphing for the lasers

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -6,12 +6,18 @@ public class LaserController : MonoBehaviour
     public float m_uptime;
     public float m_downtime;
     public float m_offset;
-
+    public float m_telegraph;
+    
     GameObject m_laserObj;
+    GameObject m_laserTeleObj;
+    
     
     void Start() {
         m_laserObj = transform.GetChild(0).gameObject;
+        m_laserTeleObj = transform.GetChild(1).gameObject;
         m_laserObj.SetActive(false);
+        m_laserTeleObj.SetActive(false);
+        
         StartCoroutine(FireLaser());
     }
 
@@ -20,13 +26,18 @@ public class LaserController : MonoBehaviour
         yield return new WaitForSeconds(m_offset);
 
         while (true) {
+            // Activate the telegraph for a certain amount of time
+            m_laserTeleObj.SetActive(true);
+            yield return new WaitForSeconds(m_telegraph);
+            
             // Activate laser for specified interval
             m_laserObj.SetActive(true);
+            m_laserTeleObj.SetActive(false);
             yield return new WaitForSeconds(m_uptime);
 
             // Deactivate laser for specified interval
             m_laserObj.SetActive(false);
-            yield return new WaitForSeconds(m_downtime);
+            yield return new WaitForSeconds(m_downtime - m_telegraph);
         }
     }
 }
