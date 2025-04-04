06ca91550c0764942cba46ff304cacd09d3a0ab1
Constantinos Vafias <cv2134@nyu.edu>
2025-04-04 00:44:08 -0400

    Tutorial Complete
    Added Text boxes based on collision, and the bonus for the level

--- Assets/Scripts/MakeTextAppear.cs
+++ Assets/Scripts/MakeTextAppear.cs
@@ -1,22 +1,37 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using TMPro;
 
 public class MakeTextAppear : MonoBehaviour
 {
     public Image[] uiImages;  // Array to hold references to different images
+    public TextMeshProUGUI[] uiTexts;    // Array to hold references to the text components
     private Dictionary<string, Image> imageDictionary;
+    private Dictionary<string, TextMeshProUGUI> textDictionary;
 
     void Start()
     {
         imageDictionary = new Dictionary<string, Image>();
-        imageDictionary.Add("TextCollision1", uiImages[0]);
-        imageDictionary.Add("TextCollision2", uiImages[1]);
-        imageDictionary.Add("TextCollision3", uiImages[2]);
+        textDictionary = new Dictionary<string, TextMeshProUGUI>();
 
+
+        // Initialize the dictionaries
+        for (int i = 0; i < uiImages.Length; i++)
+        {
+            imageDictionary.Add("TextCollision" + (i + 1), uiImages[i]);
+            textDictionary.Add("Text" + (i + 1), uiTexts[i]);
+        }
+
+        // Disable both images and text initially
         foreach (Image image in uiImages)
         {
             image.enabled = false;
+        }
+
+        foreach (TextMeshProUGUI text in uiTexts)
+        {
+            text.enabled = false;
         }
     }
 
@@ -25,7 +40,20 @@ public class MakeTextAppear : MonoBehaviour
         if (imageDictionary.ContainsKey(other.name))
         {
             Debug.Log(other.name);
+            // Enable the image and text for the corresponding collision
             imageDictionary[other.name].enabled = true;
+            if (other.name == "TextCollision1")
+            {
+                textDictionary["Text1"].enabled = true;
+            }
+            else if (other.name == "TextCollision2")
+            {
+                textDictionary["Text2"].enabled = true;
+            }
+            else
+            {
+                textDictionary["Text3"].enabled = true;
+            }
         }
     }
 
@@ -34,6 +62,11 @@ public class MakeTextAppear : MonoBehaviour
         foreach (Image image in uiImages)
         {
             image.enabled = false;
+        }
+
+        foreach (TextMeshProUGUI text in uiTexts)
+        {
+            text.enabled = false;
         }
     }
 }
