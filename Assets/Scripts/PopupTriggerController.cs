6e970ab446fbbd273959fa10f67106924ad82821
Robotgiggle <purplemrb12@gmail.com>
2025-04-23 15:57:50 -0400

    Use space instead of button to close tutorial popups

--- Assets/Scripts/PopupTriggerController.cs
+++ Assets/Scripts/PopupTriggerController.cs
@@ -4,7 +4,13 @@ public class PopupTriggerController : MonoBehaviour {
     public TutorialController m_tutorialController;
     public int m_index;
 
+    GameManager m_gameManager;
     bool m_activated;
+
+    void Start() {
+        m_gameManager = GameManager.TheInstance;
+        m_gameManager.m_resetLevelEvent.AddListener(() => m_activated = false);
+    }
 
     void OnTriggerEnter2D() {
         if (!m_activated) {
--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -5,20 +5,29 @@ public class TutorialController : MonoBehaviour {
     public PauseMenuController m_PMC;
     
     GameManager m_gameManager;
+    int m_activePopup = -1;
     
     void Start() {
         m_gameManager = GameManager.TheInstance;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && m_gameManager.m_inputsLocked) {
+            HidePopup();
+        }
     }
 
     public void ShowPopup(int index) {
         StartCoroutine(m_PMC.FadeDarkness(true));
         m_gameManager.m_inputsLocked = true;
         m_popups[index].SetActive(true);
+        m_activePopup = index;
     }
 
-    public void HidePopup(int index) {
+    public void HidePopup() {
         StartCoroutine(m_PMC.FadeDarkness(false));
         m_gameManager.m_inputsLocked = false;
-        m_popups[index].SetActive(false);
+        m_popups[m_activePopup].SetActive(false);
+        m_activePopup = -1;
     }
 }
