696e8fe4e0eebc4646bd91779113db060cc472f9
Robotgiggle <purplemrb12@gmail.com>
2025-05-02 18:32:34 -0400

    Add stub for fight manager

---
+++ Assets/Scripts/BossFightManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BossFightManager : MonoBehaviour {
+    public GameObject m_phase2Floor;
+    public GameObject m_phase2Seal;
+    public GameObject m_phase3Floor;
+    public GameObject m_phase3Seal;
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
